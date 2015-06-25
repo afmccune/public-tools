6ec113877c39bb7be0fe37d45b5e6481dac29524
Adam McCune <afmccune@email.unc.edu>
2015-06-25 09:38:34 -0400

    Author

---
+++ bq-xmlcheck/author.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+	<?php
+	require('include/functions.php');
+	require('include/head.php');
+	?>
+	<body>
+       <div id="outer">
+				
+			<div id="content">
+				<div id="content-inner">
+					<div id="issue-heading">
+						<div class="issue-heading-inner">
+							<h1>Checking for errors</h1>
+						</div>
+					</div>
+					<div id="main">
+						<div id="articles-reviews-index">
+			<?php
+				
+			$docsXml = array();
+			
+			foreach (new DirectoryIterator("../../bq/docs/") as $fn) {
+				if (preg_match('/[0-9]{1,2}.[0-9]{1}[-a-z0-9]{0,3}.[-a-z0-9]{1,20}.xml/', $fn->getFilename())) {
+					$fn_t = array();
+					$fn_t['fn'] = $fn->getFilename();	
+					
+					$fileParts = explode('.', $fn_t['fn']);
+					//$fn_t['volIss'] = $fileParts[0].'.'.$fileParts[1];
+					$fn_t['file'] = implode('.', array($fileParts[0], $fileParts[1], $fileParts[2]));
+					$fn_t['volNum'] = $fileParts[0];
+					$fn_t['issueNum'] = $fileParts[1];
+					$fn_t['issueShort'] = substr($fn_t['issueNum'], 0, 1);
+					$fn_t['fileSplit'] = $fileParts[2];
+
+					$FullXML = simplexml_load_file('../../bq/docs/'.$fn_t['fn']); 
+					
+					$XMLdocAuthors = $FullXML->xpath('//text//docAuthor');
+					$fn_t['docAuthors'] = $XMLdocAuthors; //array
+					$XMLdocAuthorsNames = $FullXML->xpath('//text//docAuthor/name');
+					$fn_t['docAuthorsNames'] = $XMLdocAuthorsNames; //array
+
+					$XMLauthors = $FullXML->xpath('//teiHeader/fileDesc/titleStmt/author');
+					$fn_t['authors'] = $XMLauthors; //array
+					$XMLauthorsLast = $FullXML->xpath('//teiHeader/fileDesc/titleStmt/author/@n');
+					$fn_t['authorsLast'] = $XMLauthorsLast; //array
+					
+					$fn_t['errors'] = array();
+					if(count($fn_t['docAuthors']) > 0) {
+						if(count($fn_t['authors']) < count($fn_t['docAuthors'])) {
+							$fn_t['errors'][] = "Fewer header authors than in-text docAuthors.";
+						}
+						
+					}
+					
+					$docsXml[] = $fn_t;
+	
+					
+				}
+			}
+						
+			for ($i=0; $i<count($docsXml); $i++) {
+				print '<h4><a href="/bq/'.$docsXml[$i]['file'].'">'.$docsXml[$i]['file'].'</a></h4>';
+				foreach($docsXml[$i]['errors'] as $error) {
+					print '<p>'.$error.'</p>';
+				}
+			}
+			?>
+							
+						</div> <!-- #articles-reviews-index -->
+					</div> <!-- #main -->
+				</div> <!-- #content-inner -->
+			</div> <!-- #content -->
+		</div> <!-- #outer -->
+	</body>
+</html>
+
