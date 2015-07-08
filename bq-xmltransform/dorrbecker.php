4123106323a03ef5fbb373b69c37a6516be4dbac
Adam Forrest McCune (afmccune) <afmccune@email.unc.edu>
2015-07-08 17:03:38 -0400

    updated solrFile and schema, dorrbecker fix

---
+++ bq-xmltransform/dorrbecker.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<?php
+require('include/functions.php');
+	
+$nl = "
+";
+?>
+	<body>
+        <div id="outer">
+				
+			<div id="content">
+				<div id="content-inner">
+					<div id="issue-heading">
+						<div class="issue-heading-inner">
+							<h1>XML Transform (Dörrbecker)</h1>
+						</div>
+					</div>
+					<div id="main">
+						<div id="allIssues">
+			
+						<?php
+						
+					$docsXml = array(); 
+					foreach (new DirectoryIterator("../../bq/docs/") as $fn) {
+						if (preg_match('/[0-9]{1,2}.[0-9]{1}[-a-z0-9]{0,3}.[-a-z0-9]{1,20}.xml/', $fn->getFilename())) {
+							$fn_t = array();
+							$fn_t['fn'] = $fn->getFilename();	
+							
+							$fileParts = explode('.', $fn_t['fn']);
+							//$fn_t['volIss'] = $fileParts[0].'.'.$fileParts[1];
+							$fn_t['file'] = implode('.', array($fileParts[0], $fileParts[1], $fileParts[2]));
+							$fn_t['volNum'] = $fileParts[0];
+							$fn_t['issueNum'] = $fileParts[1];
+							$fn_t['issueShort'] = substr($fn_t['issueNum'], 0, 1);
+							$fn_t['fileSplit'] = $fileParts[2];
+
+							$XMLstring = file_get_contents('../../bq/docs/'.$fn_t['fn']);
+							$XMLstringNew = $XMLstring;
+							
+							if(preg_match("/Börrbecker/", $XMLstring)) {
+								$XMLstringNew = preg_replace("/Börrbecker/", "Dörrbecker", $XMLstringNew);
+							
+								file_put_contents('new/'.$fn_t['fn'], $XMLstringNew);
+								echo '<p>Converted '.$fn_t['fn'].'</p>';
+							}
+
+						}	
+					}
+						
+						?>
+						</div> <!-- #allIssues -->
+					</div> <!-- #main -->
+				</div> <!-- #content-inner -->
+			</div> <!-- #content -->
+		</div> <!-- #outer -->
+	</body>
+</html>
+
