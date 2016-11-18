d299956027301fa0080a4fa4dc4a361191cca1e3
Adam F McCune <afmccune@email.unc.edu>
2016-11-18 15:26:29 -0600

    working on erdman

---
+++ wba-erdman/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<?php
+require('include/functions.php');
+	
+$nl = "
+";
+
+$replace = array();
+$replace[''] = '';
+// $replace['<pb n="([0-9]{1,})"/>)'] = '</page><page n="$1">'; 
+// This won't work: it will create overlapping tags because
+// <pb> is just stuck wherever inside of <div>s.
+// Actually, that right there sinks the whole thing. 
+// <pb>s inside of <div>s.
+
+
+?>
+	<body>
+        <div id="outer">
+				
+			<div id="content">
+				<div id="content-inner">
+					<div id="issue-heading">
+						<div class="issue-heading-inner">
+							<h1>Transform Erdman</h1>
+						</div>
+					</div>
+					<div id="main">
+						<div id="allIssues">
+			
+						<?php
+						
+					$docsXml = array(); 
+					foreach (new DirectoryIterator("../../bq/docs/") as $fn) {
+						if (preg_match('/.xml/', $fn->getFilename())) {
+							$fn_t = array();
+							$fn_t['fn'] = $fn->getFilename();	
+							
+							$fileParts = explode('.', $fn_t['fn']);
+							//$fn_t['volIss'] = $fileParts[0].'.'.$fileParts[1];
+							$fn_t['file'] = implode('.', array($fileParts[0], $fileParts[1], $fileParts[2]));
+							$fn_t['volNum'] = $fileParts[0];
+							$fn_t['issueNum'] = $fileParts[1];
+							$fn_t['issueShort'] = substr($fn_t['issueNum'], 0, 1);
+							//$fn_t['fileSplit'] = $fileParts[2];
+
+							$XMLstring = file_get_contents('../../bq/docs/'.$fn_t['fn']);
+							$XMLstringNew = $XMLstring;
+							
+							foreach($replace as $key => $value) {
+								$XMLstringNew = preg_replace("@".$key."@", "".$value."", $XMLstringNew);
+							}
+							
+							if($XMLstring !== $XMLstringNew && $XMLstringNew !== '') {
+								file_put_contents('new/'.$fn_t['fn'], $XMLstringNew);
+								echo '<h4>Converted '.$fn_t['fn'].'</h4>';
+							} else if ($XMLstringNew == '') {
+								echo '<p style="color: red;">ERROR: transformed '.$fn_t['fn'].' is blank.</p>';
+							} else {
+								echo '<p>'.$fn_t['fn'].': no change</p>';
+							}
+
+						}	
+					}
+						
+						?>
+						</div> <!-- #allIssues -->
+					</div> <!-- #main -->
+				</div> <!-- #content-inner -->
+			</div> <!-- #content -->
+		</div> <!-- #outer -->
+	</body>
+</html>
+
