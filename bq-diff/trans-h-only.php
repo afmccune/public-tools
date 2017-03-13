42f0a31bf037fed0dad361ed29a7a3b5aab3f3e4
Adam F McCune <afmccune@email.unc.edu>
2017-03-13 16:42:10 -0500

    HTML replace, diff

---
+++ bq-diff/trans-h-only.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<?php
+require('include/functions.php');
+	
+$nl = "
+";
+?>
+	<body>
+        <div id="outer">
+				
+			<div id="content">
+				<div id="content-inner">
+					<div id="issue-heading">
+						<div class="issue-heading-inner">
+							<h1>HTML Diff Guide (check all - and only - transformed files against originals)</h1>
+						</div>
+					</div>
+					<div id="main">
+						<div id="allIssues">
+			
+						<?php
+						
+						$docsHtml = array(); 
+						foreach (new DirectoryIterator("../bq-htmltransform/new/") as $fn) {
+							if (preg_match('/.html/', $fn->getFilename())) {
+								$fn_t = array();
+								
+								$fn_t['fn'] = $fn->getFilename();	
+								$fn_t['file'] = str_replace('.html', '', $fn_t['fn']);
+								
+								echo '<p><a target="_blank" href="trans-h-diff.php?file='.$fn_t['file'].'">'.$fn_t['file'].'</a></p>';
+							}	
+						}
+						
+						?>
+						</div> <!-- #allIssues -->
+					</div> <!-- #main -->
+				</div> <!-- #content-inner -->
+			</div> <!-- #content -->
+		</div> <!-- #outer -->
+	</body>
+</html>
+
